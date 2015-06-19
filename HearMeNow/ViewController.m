ea67b4c1c8de5b3d0f2a5c97c32475458458d733
Edgar Martinez <edgarmtz@MacBook-Air-de-Andres.local>
2015-06-18 19:37:51 -0500

    Comment added

--- HearMeNow/ViewController.m
+++ HearMeNow/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by Edgar Martinez on 17/06/15.
 //  Copyright (c) 2015 Edgar Martinez. All rights reserved.
-//
+// This is the view controller implementation file
 
 #import "ViewController.h"
 
